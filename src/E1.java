4568a7da2667d11592c9e3d266920542c0f35541
David <dav.llwyr@gmail.com>
2021-06-10 21:40:21 -0600

    Repo created

---
+++ src/E1.java
@@ -0,0 +1,13 @@
+
+public class E1{
+    public static void main(String[] args) {
+        E1 e1 = new E1();
+    }
+    //Define the methods
+    int[] numbers = new int[5];
+    Scanner scanner = new Scanner(System.in);
+
+
+
+
+}
